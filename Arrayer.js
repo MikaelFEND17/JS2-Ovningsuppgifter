438ac03206f151f0ae83f4c40115ce971af87e4a
MikaelFEND17 <Mikael.Larsson2@yh.nackademin.se>
2018-02-22 12:32:23 +0100

    Update

---
+++ Arrayer.js
@@ -0,0 +1,231 @@
+//1
+
+var bilar = [ "Volvo", "BMW", "Audi", "Skoda",
+"Toyota", "Ford", "Mercedes","Seat", "Honda",
+"Volkswagen","Opel", "Mazda","Suzuki" ]; 
+
+//a. Ta fram alla bilar som har mer än 5 tecken i namnet.
+const bilarLength = bilar.filter(bilar => bilar.length > 5);
+
+console.log(bilarLength);
+
+
+//b. Ta fram alla bilar vars namn börjar på ”V”.
+const vBilar = bilar.filter(function(bil) { return bil.substr(0, 1) === "V";}); 
+
+console.log(vBilar);
+
+function filter(array, index, letter) {
+    var filteredValues = array.filter(function(element) {
+       return element.charAt(index) === letter;
+    });
+    return filteredValues;
+}
+
+console.log(filter(bilar, 0, "V"));
+
+//c. Ta fram alla bilar vars namn innehåller bokstäverna ”da”.
+
+const daBilar = bilar.filter(function (item) { return item.indexOf("da") >= 0; });
+
+console.log(daBilar)
+
+function arrContainsSubString(array, substr)
+{ 
+    var filteredValues = array.filter(function(element) {
+        return element.indexOf(substr) >= 0;
+    });
+    return filteredValues;
+}
+
+console.log(arrContainsSubString(bilar, "da"));
+
+
+//d. Ta fram alla bilar som börjar på ”M” eller som slutar på ”i” 
+
+const miBilar = bilar.filter(function(bil) { return bil.substr(0, 1) === "M" || bil.substr(bil.length-1) === "i";});
+
+console.log(miBilar);
+
+//2
+
+var bands = [ "ACDC", "Queen", "Aerosmith", "Iron Maiden", "Megadeth",
+"Metallica", "Pearl Jam", "Oasis", "Kiss", "Blur", "Eurythmics", "Genesis",
+"INXS", "Midnight Oil", "Kent", "Madness", "Manic Street Preachers", "The Offspring", "Pink Floyd", "Rammstein", "Red Hot Chili Peppers", "Deep Purple",
+"U2"]; 
+
+//a. Ta fram det band som har längst bandnamn
+var longest = bands.sort(function (a, b) { return b.length - a.length; })[0];
+console.log(longest);
+
+var longest = bands.reduce(function (a, b) { return a.length > b.length ? a : b; });
+console.log(longest);
+
+//b. Ta fram en siffra på hur många band som börjar på bokstaven ”M”
+var numOfMBands = bands.filter(function(item) { return item[0] === "M"; }).length
+
+console.log(numOfMBands);
+
+//c. Ta ut en lista på alla band sorterad i bokstavsordning. Visa bara band som har ett bandnamn som är längre än 10 bokstäver
+//const sortedBands = bands.sort();
+const bandsSorttedLongerThanTen = bands.filter(bands => bands.length > 10).sort();
+
+console.log(bandsSorttedLongerThanTen);
+
+//d. Skapa en lista som sorteras på längden på banden namn. Det band med längst namn skall hamna först i listan och det med kortast namn skall hamna sist. 
+
+const sortByLongestName = bands.sort(function(a, b) { return b.length - a.length });
+
+console.log(sortByLongestName);
+
+//3 
+
+var integers = [ 54, 23, 76, 123, 93, 7, 3489, 88 ]; 
+
+//a. Ta fram medelvärdet av alla tal.
+
+const medel = integers.reduce(function(total, value, index, array) { total += value; if (index === array.length-1) { return total/array.length; } else { return total; } });
+console.log(medel);
+
+//b. Ta fram det största av alla tal.
+
+const biggest = integers.sort(function (a, b) { return b - a; })[0];
+console.log(biggest);
+
+//c. Beräkna summan av alla tal.
+
+const sum = integers.reduce((total, value) => total + value);
+console.log(sum);
+
+//d. Ta fram summan av alla tal större än 70
+
+const sum70plus = integers.reduce(function(total, value) { if (value > 70) { return total + value; } else { return total; } });
+console.log(sum70plus);
+
+//e. Ta fram alla jämna tal .
+const even = integers.filter(function(num) { if (num % 2 === 0) { return num; }});
+
+console.log(even);
+
+//f. Skapa en ny array från denna array där alla värden ökats med 2 dvs nya listan blir 56,25,78,125 osv. 
+
+integers = [ 54, 23, 76, 123, 93, 7, 3489, 88 ]; 
+const newIntegers = integers.map(integer => integer + 2);
+console.log(newIntegers);
+
+
+var countries = [
+  {
+    name: "United Kingdom",
+    continent: "Europe",
+    population: 65270121,
+    pFemale: 0.508
+  },
+  {
+    name: "Republic of Ireland",
+    continent: "Europe",
+    population: 4708209,
+    pFemale: 0.499
+  },
+  {
+    name: "Australia",
+    continent: "Oceania",
+    population: 24482205,
+    pFemale: 0.502
+  },
+  {
+    name: "Taiwan",
+    continent: "Asia",
+    population: 23522296,
+    pFemale: 0.501
+  },
+  {
+    name: "Uruguay",
+    continent: "South America",
+    population: 3446772,
+    pFemale: 0.517
+  },
+  {
+    name: "Morocco",
+    continent: "Africa",
+    population: 35010005,
+    pFemale: 0.51
+  },
+  {
+    name: "Nigeria",
+    continent: "Africa",
+    population: 188688090,
+    pFemale: 0.494
+  },
+  {
+    name: "Zimbabwe",
+    continent: "Africa",
+    population: 16051510,
+    pFemale: 0.507
+  },
+  {
+    name: "China",
+    continent: "Asia",
+    population: 1381321335,
+    pFemale: 0.488
+  },
+  {
+    name: "Mexico",
+    continent: "North America",
+    population: 129386794,
+    pFemale: 0.507
+  },
+  {
+    name: "Canada",
+    continent: "North America",
+    population: 36446796,
+    pFemale: 0.504
+  },
+  {
+    name: "Czech Republic",
+    continent: " Europe ",
+    population: 10556351,
+    pFemale: 0.509
+  },
+  {
+    name: "Iceland",
+    continent: "Europe",
+    population: 332631,
+    pFemale: 0.496
+  }
+]; 
+
+//a. Skriv ut namnen på alla länder i arrayen.
+
+const countryNames = countries.map(country => country.name);
+console.log(countryNames);
+
+//b. Hur många bor i hela (arrayen) världen?
+
+const totalPopulation = countries.reduce((total, value) => total + value.population, 0);
+console.log(totalPopulation);
+
+//c. Hur många bor i Europa?
+
+const europePopulation = 0;
+console.log(europePopulation);
+
+//d. Vilket land har minst befolkning i världen?
+
+const smallestPopulation = "None";
+console.log(smallestPopulation);
+
+//e. Vad är medelbefolkningen för länder i Afrika?
+
+const averageAfrica = 0;
+console.log(averageAfrica);
+
+//f. Finns det något land som har mer än 50 000 000 invånare?
+
+const countryAboveFiftyMillion = 0;
+console.log(countryAboveFiftyMillion)
+
+//g. Vilka länder har mellan 8 och 15 miljoner invånare?
+
+const countriesBetweenEightAndFifteen = 0;
+console.log(countriesBetweenEightAndFifteen);
---
+++ JSONWebApi.js
@@ -0,0 +1,85 @@
+var input = {
+  fornamn: "Lisa",
+  efternamn: "Lind",
+  adress: {
+    gata: "Storgatan 3",
+    postort: "Södertälje",
+    postnr: "11122"
+  },
+  kurser: ["Javascript1", "Javascript2", "Webbdesign"]
+};
+
+//1. Du har följande json sträng. Ta emot strängen i en variabel och skriv sedan ut förnamn och efternamn med console.log
+console.log(input.fornamn + " " + input.efternamn)
+
+//2. Ta sedan fram alla kurser ur strängen och skriv ut med console.log
+console.log(JSON.stringify(input.kurser));
+
+//3. Ta ut alla adress uppgifter och skriv ut med console.log
+console.log("Gata: " + input.adress.gata + " Postort: " + input.adress.postort + " Postnummer: " + input.adress.postnr);
+
+//4. Koppla nu det du gjort i uppgift 1-3 till en webbsida. Du skall skapa element på sidan där du
+//   lägger värdena från json-filen. Tänk dig att du öppnar learnpoint och visar en översiktssida över
+//   en student. Lägg alla kurser i en lista. Välj vilken typ av element du visar de andra värdena med.
+
+var divWrapper = document.getElementById("wrapper");
+
+var studentName = document.createElement("p");
+var studentNameText = document.createTextNode(input.fornamn + " " + input.efternamn);
+studentName.appendChild(studentNameText);
+
+divWrapper.appendChild(studentName);
+
+
+var studentAdress = document.createElement("p");
+var studentAdressText = document.createTextNode("Gata: " + input.adress.gata + " Postort: " + input.adress.postort + " Postnummer: " + input.adress.postnr);
+studentAdress.appendChild(studentAdressText);
+
+divWrapper.appendChild(studentAdress);
+
+var kurser = document.createElement("ul");
+
+for (kurs of input.kurser)
+{
+    console.log(kurs);
+    let liKurs = document.createElement("li");
+    let liKursText = document.createTextNode(kurs);
+    liKurs.appendChild(liKursText);
+    kurser.appendChild(liKurs);
+}
+
+divWrapper.appendChild(kurser);
+
+
+//5. Skapa en klass som du kallar för produkt . Den skall ha egenskaperna namn, pris och beskrivning.
+//   Skapa upp en instans av klassen och sätt alla värden. Gör sedan om det till en json sträng och
+//   skriv ut texten i json format via console.log.
+
+function Product(aName, aPrice, aDescription)
+{
+    this.name = aName;
+    this.price = aPrice;
+    this.description = aDescription
+}
+
+let product1 = new Product("Test", 199.99, "Test Product");
+
+console.log(JSON.stringify(product1));
+
+//6. Ta emot följande json-sträng och skriv ut en klasslista på en webbsida med en rubrik med
+//   klassens namn och en lista med alla studenter
+var klass = {
+  className: "FEND17",
+  students: [
+    { name: "Lisa Lind", email: "Lisalind@gmail.com" },
+    { name: "Kalle Karlsson", email: "kalleK@gmail.com" },
+    { name: "Anna Persson", email: "annaP@gmail.com" }
+  ]
+};
+
+console.log(klass.className);
+
+for (student of klass.students)
+{
+    console.log(student.name + " " + student.email);
+}
